509f235b6b2b12475dd4acb9451cbbbbc1dbb240
Ryo Moriwaki <ryomo@duck.com>
2024-12-18 11:47:08 +0900

    Minor fix

--- webroot/script.js
+++ webroot/script.js
@@ -6,7 +6,7 @@ import { Puzzle } from './puzzle.js'
  * The default puzzle number to start with.
  * This will be overridden by `DevvitMessenger._receiveMessage()` .
  */
-const defaultPuzzleNum = 5;
+const defaultPuzzleNum = 1;
 
 const dom = new Dom();
 const puzzle = new Puzzle(dom);

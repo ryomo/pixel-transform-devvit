daf15f64a0a83449f2652d287a2ad97d5364c671
Ryo Moriwaki <ryomo@duck.com>
2024-12-18 11:05:46 +0900

    Add new puzzles to puzzles.json, making a total of 10 puzzles

--- webroot/script.js
+++ webroot/script.js
@@ -2,8 +2,15 @@ import { DevvitMessenger } from './devvitMessenger.js';
 import { Dom } from './dom.js';
 import { Puzzle } from './puzzle.js'
 
+/**
+ * The default puzzle number to start with.
+ * This will be overridden by `DevvitMessenger._receiveMessage()` .
+ */
+const defaultPuzzleNum = 5;
+
 const dom = new Dom();
 const puzzle = new Puzzle(dom);
+puzzle.puzzleIndex = defaultPuzzleNum - 1;
 const devvitMessenger = new DevvitMessenger(puzzle);
 
 await puzzle.init();
